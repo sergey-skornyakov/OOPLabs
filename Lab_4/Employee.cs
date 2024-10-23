368b4965927842b0f2404db8fa904ef5b0b97287
Андрей Боев <a.boev.it@gmail.com>
2024-10-23 16:59:59 +0300

    Лабораторная работа №4

---
+++ Lab_4/Employee.cs
@@ -0,0 +1,31 @@
+﻿namespace Lab_4;
+
+public class Employee
+{
+    public string FirstName { get; set; }
+
+    private string lastName;
+    public string LastName
+    {
+        get { return lastName; }
+        set { lastName = value; }
+    }
+
+    private int skill;
+    public int Skill
+    {
+        get => skill;
+        set
+        {
+            if (skill >= 0)
+                skill = value;
+        }
+    }
+
+    public bool IsBest => Skill < 25;
+
+    public string GetInfo() => GetInfo(true);
+
+    public string GetInfo(bool includeIntProperty) =>
+        $"{FirstName} {lastName}{(includeIntProperty ? $" {Skill}" : "")} {IsBest}";
+}
---
+++ Lab_4/Program.cs
@@ -0,0 +1,17 @@
+﻿using Lab_4;
+
+namespace Lab_4
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.FirstName = "Иван";
+            employee.LastName = "Иванов";
+            employee.Skill = 20;
+
+            Console.WriteLine(employee.GetInfo());
+        }
+    }
+}

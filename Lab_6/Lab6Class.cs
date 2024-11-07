9a930379229eab4d3e0b1a0ed38d0216046b8286
Андрей Боев <a.boev.it@gmail.com>
2024-11-07 14:43:45 +0300

    Лабораторная работа №6

---
+++ Lab_6/Lab6Class.cs
@@ -0,0 +1,76 @@
+﻿namespace Lab_6;
+
+public class Lab6Class
+{
+    public string Str1
+    {
+        // 11.
+        get => str1!;
+        set => str1 = value;
+    }
+
+    public string? Str2
+    {
+        get => str2;
+        set => str2 = value;
+    }
+
+    public int Num1
+    {
+        get => num1;
+        set => num1 = value;
+    }
+
+    public int? Num2
+    {
+        get => num2;
+        set => num2 = value;
+    }
+
+    public double DNum1
+    {
+        get => dNum1;
+        set => dNum1 = value;
+    }
+
+    public double? DNum2
+    {
+        get => dNum2;
+        set => dNum2 = value;
+    }
+
+    public int Length
+    {
+        get => length;
+        set => length = value;
+    }
+
+    private string str1;
+    private string? str2;
+    private int num1;
+    private int? num2;
+    private double dNum1;
+    private double? dNum2;
+
+    // 6.
+    private int length;
+
+    public Lab6Class(string str1, string? str2, int num1, int? num2, double dNum1, double? dNum2)
+    {
+        this.str1 = str1;
+        this.str2 = str2;
+        this.num1 = num1;
+        this.num2 = num2;
+        this.dNum1 = dNum1;
+        this.dNum2 = dNum2;
+
+        // 6.
+        length = str1!.Length;
+    }
+
+    public override string ToString()
+    {
+        return $"{str1} {str2 ?? "Данные отсутствуют"} {num1} {(num2 != null ? num2.ToString() : "Данные отсутствуют")} " +
+               $"{dNum1} {(dNum2 != null ? dNum2.ToString() : "Данные отсутствуют")}";
+    }
+}
---
+++ Lab_6/Program.cs
@@ -0,0 +1,51 @@
+﻿using Lab_6;
+
+namespace Lab_6
+{
+    static class Program
+    {
+        // 8.
+        public static void AssignValueStr1(Lab6Class instance)
+        {
+            instance.Str1 ??= "Empty string";
+            instance.Num2 ??= 0;
+            instance.DNum2 ??= 0.0;
+        }
+
+        // 9.
+        public static string GetPropertyValueWithNullableInstance(Lab6Class? instance)
+        {
+            return instance?.Str1 ?? "String";
+        }
+
+        // 10.
+        public static string GetPropertyValueWithNullableProperty(Lab6Class instance)
+        {
+            return instance.Str1 ?? "String";
+        }
+
+        static void Main(string[] args)
+        {
+            Lab6Class  lab6Class1 = new Lab6Class("Text 1", "Text 2", 1, 2, 5.1, 4.3);
+            Lab6Class  lab6Class2 = new Lab6Class("Text 1", null, 1, 2, 5.2, 4.3);
+            Lab6Class  lab6Class3 = new Lab6Class("Text 1", "Text 2", 1, null, 5.3, 4.3);
+            Lab6Class  lab6Class4 = new Lab6Class("Text 1", "Text 2", 1, 2, 5.4, null);
+            Console.WriteLine(lab6Class1);
+            Console.WriteLine(lab6Class2);
+            Console.WriteLine(lab6Class3);
+            Console.WriteLine(lab6Class4);
+
+            Console.WriteLine("5.1 Если присвоить null свойству типа string, которое не может принимать null, то строка " +
+                              "автоматически примет пустое значение.");
+            Console.WriteLine("5.2 Если написать после null восклицательный знак, то будут подавляться предупреждения о " +
+                              "возможном null значении.");
+            Console.WriteLine("6.1 Если присвоить null значение переменной типа int или double, то возникает ошибка на " +
+                              "этапе компиляции о невозможности преобразовать null в int или double. Тот же эффект будет " +
+                              "и при использовании восклицательного знака.");
+            Console.WriteLine("7. ---");
+            Console.WriteLine("7. ");
+
+
+        }
+    }
+}
